f28efb1c4eb58186bfceab7e9502434217a79cf1
Vi-green <victoriagreenco@gmail.com>
2026-04-08 10:46:38 -0300

    new file:   cloudbuild.yml 	modified:   models/staging/stg_ventas.sql

--- models/staging/stg_ventas.sql
+++ models/staging/stg_ventas.sql
@@ -1,8 +1,6 @@
 with
 
 source as (
-
-    -- {# This references seed (CSV) data - try switching to {{ source('ecom', 'raw_customers') }} #}
     select * from {{ source('ventas', 'ventas') }}
 
 ),
@@ -15,8 +13,7 @@ renamed as (
         cod_cliente as customer_id,
 
         ---------- text
-        cliente as nombre_fantasia_test /*por favor que me lo comitee, que le pasa a esa pelona? Intento sin build automatico*/
-
+        cliente as nombre_fantasia_test 
     from source
 
 )
